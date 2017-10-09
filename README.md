abf9ddad3c77caf70171047738e1a35fef554df7
Lloyd King, Jr <lking3497@gmail.com>
2017-10-08 17:52:21 -0700

    Aracde game

---
+++ README.md
@@ -0,0 +1,17 @@
+frontend-nanodegree-arcade-game
+===============================
+## Classic Frogger Game
+
+###Description
+This is a frogger inspired arcade game. You are a horn girl who wants to cross the road to reach the river but
+there are some disrespectful lady bugs who want to stop you (no one knows why).
+
+###Instructions
+To play the game download all the files and open index.html in your browser.
+
+###How to play
+Use the arrow keys to move the character.
+Avoid hitting the lady bugs while trying to make it to the river on the other side of the stone road. When you reach the water your character will reset back to the grass so that you can try again.
+
+###Aditional notes
+There will be updates coming soon with collectibles, new obstacles, new enemies, and a soundtrack inspired by 90s video game soundtracks.
---
+++ js/app.js
@@ -0,0 +1,121 @@
+// Enemies our player must avoid
+var Enemy = function(x, y, speed) { "use strict";
+    // Variables applied to each of our instances go here,
+    // we've provided one for you to get started
+    this.x = x;
+    this.y = y;
+    this.speed = speed;
+
+    // The image/sprite for our enemies, this uses
+    // a helper we've provided to easily load images
+    this.sprite = 'images/enemy-bug.png';
+};
+
+// Update the enemy's position, required method for game
+// Parameter: dt, a time delta between ticks
+Enemy.prototype.update = function(dt) { "use strict";
+    // You should multiply any movement by the dt parameter
+    // which will ensure the game runs at the same speed for
+    // all computers.
+    this.x += this.speed * dt;
+
+    // when off canvas, reset position of enemy to move across again
+    if (this.x > 600) {
+        this.x = -100;
+        this.speed = 100 + Math.floor(Math.random() * 512);
+    }
+
+    // Check for collision between player and enemies
+    if (player.x < this.x + 60 &&
+        player.x + 37 > this.x &&
+        player.y < this.y + 25 &&
+        30 + player.y > this.y) {
+        player.x = 252;
+        player.y = 380;
+    }
+};
+
+// Draw the enemy on the screen, required method for game
+Enemy.prototype.render = function() { "use strict";
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+// Now write your own player class
+// This class requires an update(), render() and
+// a handleInput() method.
+var Player = function(x, y, speed) { "use strict";
+    this.x = x;
+    this.y = y;
+    this.speed = speed;
+    this.sprite = 'images/char-horn-girl.png';
+};
+
+Player.prototype.update = function() { "use strict";
+    // Prevent player from moving beyond canvas wall boundaries
+    if (this.y > 380) {
+        this.y = 380;
+    }
+
+    if (this.x > 450) {
+        this.x = 450;
+    }
+
+    if (this.x < 0) {
+        this.x = 0;
+    }
+
+    // Check for player reaching top of canvas and winning the game
+    if (this.y < 0) {
+        this.x = 252;
+        this.y = 380;
+    }
+};
+
+Player.prototype.render = function() { "use strict";
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+Player.prototype.handleInput = function(keyPress) { "use strict";
+    switch (keyPress) {
+        case 'left':
+            this.x -= this.speed + 50;
+            break;
+        case 'up':
+            this.y -= this.speed + 30;
+            break;
+        case 'right':
+            this.x += this.speed + 50;
+            break;
+        case 'down':
+            this.y += this.speed + 30;
+            break;
+    }
+};
+
+// Now instantiate your objects.
+// Place all enemy objects in an array called allEnemies
+// Place the player object in a variable called player
+var allEnemies = [];
+
+// Position "y" where the enemies will are created
+var enemyPosition = [60, 140, 220];
+var player = new Player(252, 380, 50);
+var enemy;
+
+enemyPosition.forEach(function(posY) { "use strict";
+    enemy = new Enemy(0, posY, 100 + Math.floor(Math.random() * 512));
+    allEnemies.push(enemy);
+});
+
+// This listens for key presses and sends the keys to your
+// Player.handleInput() method. You don't need to modify this.
+document.addEventListener('keyup', function(e) { "use strict";
+    var allowedKeys = {
+        37: 'left',
+        38: 'up',
+        39: 'right',
+        40: 'down'
+    };
+
+    player.handleInput(allowedKeys[e.keyCode]);
+});
